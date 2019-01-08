38880276acfd4d9eefddef941b4d9032770f46e5
Martín López <mlopez@flowingcode.com>
2019-01-08 15:42:58 -0300

    Step 2: - Added webjar dependency - Added Javascript and css imports

--- src/main/java/com/flowingcode/Timeline.java
+++ src/main/java/com/flowingcode/Timeline.java
@@ -1,6 +1,10 @@
 package com.flowingcode;
 
+import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Div;
 
+@JavaScript("/webjars/vis/4.21.0/dist/vis.js")
+@StyleSheet("/webjars/vis/4.21.0/dist/vis-timeline-graph2d.min.css")
 public class Timeline extends Div {
 }
